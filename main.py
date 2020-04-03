f9d6c327004f12ba6870ef30a4af371e2b78fa1d
Remi <dev.perrier.remi@gmail.com>
2020-04-03 16:26:46 +0200

    Add companies_persons

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import csv
 
 # CONSTANTS
 NULL_VALUES = ['', None, 'null', [], {}]
+FORBIDEN_CHARS = ['\\', '\'', '\"']
 # Nodes
 VAR_COMPANIES = ['name', 'permalink', 'crunchbase_url', 'homepage_url',
                  'blog_url', 'blog_feed_url', 'twitter_username', 'twitter_username',
                  'number_of_employees', 'founded_year', 'founded_month', 'founded_day',
                  'deadpooled_year', 'deadpooled_month', 'deadpooled_day', 'deadpooled_url',
-                 'email_address', 'phone_number', 'description', 'created_at', 'updated_at']
+                 'email_address', 'phone_number', 'description', 'created_at', 'updated_at',
+                 'total_money_raised']
 VAR_PRODUCTS = ['name', 'permalink']
 VAR_PERSONS = ['first_name', 'last_name', 'permalink']
+# Relationships
+VAR_COMPANIES_PERSONS = ['is_past', 'title']
 
 
 def create_files():
@@ -23,6 +27,8 @@ def create_files():
         'companies_products.csv', 'w', encoding='utf-8')
     companies_competitors_file = open(
         'companies_competitors.csv', 'w', encoding='utf-8')
+    companies_persons_file = open(
+        'companies_persons.csv', 'w', encoding='utf-8')
 
     # Writers and headlines
     companies_writer = csv.writer(companies_file)
@@ -41,6 +47,10 @@ def create_files():
     companies_competitors_writer = csv.writer(companies_competitors_file)
     companies_competitors_writer.writerow(
         ['companie_permalink', 'competitor_permalink'])
+
+    comapnies_persons_writer = csv.writer(companies_persons_file)
+    comapnies_persons_writer.writerow(
+        ['companie_permalink', 'person_permalink'] + VAR_COMPANIES_PERSONS)
 
     # Read data and write to csv
     # Unique identifiers
@@ -70,11 +80,16 @@ def create_files():
         # Persons
         if 'relationships' in json_companie:
             for relationship in json_companie['relationships']:
+                # Node
                 if relationship['person']['permalink'] not in persons_permalinks:
                     csv_person = format(relationship['person'], VAR_PERSONS)
                     persons_writer.writerow(csv_person)
                     persons_permalinks.append(
                         relationship['person']['permalink'])
+                # Relationship
+                csv_companie_person = [json_companie['permalink'], relationship['person']
+                                       ['permalink']] + format(relationship, VAR_COMPANIES_PERSONS)
+                comapnies_persons_writer.writerow(csv_companie_person)
 
         # Competitors
         if 'competitions' in json_companie:
@@ -90,6 +105,8 @@ def create_files():
     products_file.close()
     persons_file.close()
     companies_products_file.close()
+    companies_competitors_file.close()
+    companies_persons_file.close()
 
 
 def format(dictionary, var_array):
@@ -100,10 +117,14 @@ def format(dictionary, var_array):
             line.append('N/A')
         elif dictionary[var] in NULL_VALUES:
             line.append('N/A')
+        elif type(dictionary[var]) == str:
+            s = dictionary[var]
+            for char in FORBIDEN_CHARS:
+                s = s.replace(char, '')
+            line.append(s)
         else:
             line.append(dictionary[var])
     return line
-
 
  # Call main
 create_files()
